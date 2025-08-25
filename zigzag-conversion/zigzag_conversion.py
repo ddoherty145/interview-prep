8ffc14f50d49a93606ca10b3f0eb1b4b770affe2
David Doherty <froztydavie@MacBookAir.home>
2025-08-25 14:37:20 -0700

    complete

--- zigzag-conversion/zigzag_conversion.py
+++ zigzag-conversion/zigzag_conversion.py
@@ -5,4 +5,21 @@ class Solution(object):
         :type numRows: int
         :rType: str
         """
+        if numRows == 1:
+            return s
         
+        result = []
+        n = len(s)
+        cycle = 2 * numRows - 2
+
+        for i in range(numRows):
+            j = i
+            while j < n:
+                result.append(s[j])
+                if i != 0 and i != numRows - 1:
+                    k = j + cycle - 2 * i
+                    if k < n:
+                        result.append(s[k])
+                j += cycle
+
+        return ''.join(result)
