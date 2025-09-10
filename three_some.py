1637ee8cef7ba5241421a01859ff6e7c7cd7811f
David Doherty <froztydavie@MacBookAir.home>
2025-09-10 11:55:15 -0700

    3Sum

---
+++ three_some.py
@@ -0,0 +1,36 @@
+# Gicen an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that
+# i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+# Notice that the solution set must not contain duplicate triplets.
+
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = []
+        n = len(nums)
+        
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            left, right = i + 1, n - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        
+        return result
