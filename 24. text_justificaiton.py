b4d9070a476a70e338446b9804436cda583c4f2e
David Doherty <froztydavie@MacBookAir.home>
2025-10-07 11:41:13 -0700

    Simplify Path

---
+++ 24. text_justificaiton.py
@@ -0,0 +1,97 @@
+# Given an array of strings words and a width maxWidth, format the text such that each line has exactly maxWidth characters and is fully (left and right) justified.
+
+# You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+
+# Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line does not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+
+# For the last line of text, it should be left-justified, and no extra space is inserted between words.
+
+# Note:
+
+# A word is defined as a character sequence consisting of non-space characters only.
+# Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
+# The input array words contains at least one word.
+ 
+
+# Example 1:
+
+# Input: words = ["This", "is", "an", "example", "of", "text", "justification."], maxWidth = 16
+# Output:
+# [
+#    "This    is    an",
+#    "example  of text",
+#    "justification.  "
+# ]
+# Example 2:
+
+# Input: words = ["What","must","be","acknowledgment","shall","be"], maxWidth = 16
+# Output:
+# [
+#   "What   must   be",
+#   "acknowledgment  ",
+#   "shall be        "
+# ]
+# Explanation: Note that the last line is "shall be    " instead of "shall     be", because the last line must be left-justified instead of fully-justified.
+# Note that the second line is also left-justified because it contains only one word.
+# Example 3:
+
+# Input: words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], maxWidth = 20
+# Output:
+# [
+#   "Science  is  what we",
+#   "understand      well",
+#   "enough to explain to",
+#   "a  computer.  Art is",
+#   "everything  else  we",
+#   "do                  "
+# ]
+ 
+
+# Constraints:
+
+# 1 <= words.length <= 300
+# 1 <= words[i].length <= 20
+# words[i] consists of only English letters and symbols.
+# 1 <= maxWidth <= 100
+# words[i].length <= maxWidth
+
+class Solution(object):
+    def fullJustify(self, words, maxWidth):
+        """
+        :type words: List[str]
+        :type maxWidth: int
+        :rtype: List[str]
+        """
+        res = []
+        n = len(words)
+        i = 0
+        
+        while i < n:
+            j = i + 1
+            line_length = len(words[i])
+            
+            while j < n and line_length + len(words[j]) + (j - i) <= maxWidth:
+                line_length += len(words[j])
+                j += 1
+            
+            line = ""
+            number_of_words = j - i
+            if j == n or number_of_words == 1:
+                for k in range(i, j):
+                    line += words[k] + " "
+                line = line[:-1]
+                line += " " * (maxWidth - len(line))
+            else:
+                total_spaces = maxWidth - line_length
+                spaces_between_words = total_spaces // (number_of_words - 1)
+                extra_spaces = total_spaces % (number_of_words - 1)
+                
+                for k in range(i, j - 1):
+                    line += words[k]
+                    line += " " * (spaces_between_words + (1 if k - i < extra_spaces else 0))
+                line += words[j - 1]
+            
+            res.append(line)
+            i = j
+        
+        return res
